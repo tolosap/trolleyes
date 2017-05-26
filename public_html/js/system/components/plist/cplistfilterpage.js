53c35becc280c9346c835149907a43558ac2b444
rafa aznar <rafaaznar@gmail.com>
2017-05-26 14:43:52 +0200

    generic

---
+++ public_html/js/system/components/plist/cplistfilterpage.js
@@ -0,0 +1,33 @@
+'use strict';
+moduloDirectivas.component('cplistfilterpage', {
+    restrict: 'E',
+    bindings: {
+        url: '<',
+        fields: '<',
+        rpp: '<',
+        numpage: '<',
+        order: '<',
+        filter: '<',
+        sfilter: '<'
+    },
+    templateUrl: 'js/system/components/plist/cplistfilterpage.html',
+    controllerAs: 'cf',
+    controller: ['serverService', '$location', function (serverService, $location) {
+            var self = this;
+            self.bean={};
+            self.dofilter = function ()
+            {
+                if (self.filter && self.filteroperator && self.filtervalue) {
+                    if (self.filterParams) {
+                        self.filterExpression = self.filterParams + '+and,' + self.filter + ',' + self.filteroperator + ',' + self.filtervalue;
+                    } else {
+                        self.filterExpression = 'and,' + self.filter + ',' + self.filteroperator + ',' + self.filtervalue;
+                    }
+                    $location.path(self.url + '/' + self.numpage + '/' + self.rpp).search('filter', self.filterExpression).search('sfilter', self.sfilter).search('order', self.order);
+                }
+                return false;
+            }
+        }]
+});
+
+
