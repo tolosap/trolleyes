813d8f44291d4ba26cda150416fdc9d9ce33eb31
raznara <raznara@depinf-pc03>
2017-06-09 17:43:06 +0200

    newalumno

--- public_html/js/system/newalumno.js
+++ public_html/js/system/newalumno.js
@@ -1,13 +1,35 @@
 'use strict';
 moduloSistema.controller('NewalumnoController', ['$http', '$scope', '$routeParams', '$location', 'serverService', 'sessionService',
     function ($http, $scope, $routeParams, $location, serverService, sessionService) {
-        $scope.title = "Registro de alumno para el curso " + $routeParams.codigo;        
+        $scope.title = "Registro de alumno para el curso " + $routeParams.codigo;
         $scope.isSessionActive = sessionService.isSessionActive();
         $scope.status = null;
         $scope.debugging = serverService.debugging();
         $scope.bean = {};
         $scope.codigoGrupo = $routeParams.codigo;
 
+        $scope.fase = 1;
+
+        $scope.validausuario = function () {
+            if ($scope.fase == 1) {
+                if ($scope.bean.login) {
+                    $http.get(serverService.getAppUrl() + '?ob=usuario&op=checkloginnotexists&login=' + $scope.bean.login, 'GET', '').then(function (response) {
+                        if (response.status == 200) {
+                            if (response.data.message == "OK") {
+                                $scope.fase = 2;
+                            }
+                        } else {
+                            
+                            return false;
+                        }
+                    }, function errorCallback(response, status) {
+                        $scope.checkGrupoStatusMsg = "Error: el grupo no es correcto.";
+                        return false;
+                        return false;
+                    });
+                }
+            }
+        }
 
     }]);
 
