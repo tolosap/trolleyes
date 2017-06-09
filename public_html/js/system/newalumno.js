e8434d4a804c8164e89f91d3f01cee0c2ce8588f
raznara <raznara@depinf-pc03>
2017-06-09 19:43:23 +0200

    newalumno

--- public_html/js/system/newalumno.js
+++ public_html/js/system/newalumno.js
@@ -1,36 +1,35 @@
 'use strict';
-moduloSistema.controller('NewalumnoController', ['$http', '$scope', '$routeParams', '$location', 'serverService', 'sessionService',
-    function ($http, $scope, $routeParams, $location, serverService, sessionService) {
+moduloSistema.controller('NewalumnoController', ['$http', '$scope', '$routeParams', '$location', 'serverService', 'sessionService', '$rootScope',
+    function ($http, $scope, $routeParams, $location, serverService, sessionService, $rootScope) {
         $scope.title = "Registro de alumno para el curso " + $routeParams.codigo;
         $scope.isSessionActive = sessionService.isSessionActive();
         $scope.status = null;
         $scope.debugging = serverService.debugging();
         $scope.bean = {};
         $scope.codigoGrupo = $routeParams.codigo;
-
         $scope.fase = 1;
-
         $scope.validausuario = function () {
             if ($scope.fase == 1) {
                 if ($scope.bean.login) {
-                    $http.get(serverService.getAppUrl() + '?ob=usuario&op=checkloginnotexists&login=' + $scope.bean.login, 'GET', '').then(function (response) {
+                    $http.get(serverService.getAppUrl() + '?ob=usuario&op=checklogin&login=' + $scope.bean.login, 'GET', '').then(function (response) {
                         if (response.status == 200) {
                             if (response.data.message == "OK") {
+                                $rootScope.outerForm.login.$setValidity('repetido', false);
                                 $scope.fase = 2;
                             }
                         } else {
-                            
+                            $rootScope.outerForm.login.$setValidity('repetido', true);
                             return false;
                         }
                     }, function errorCallback(response, status) {
-                        $scope.checkGrupoStatusMsg = "Error: el grupo no es correcto.";
-                        return false;
+                        $rootScope.outerForm.login.$setValidity('repetido', true);
                         return false;
                     });
                 }
             }
         }
 
-    }]);
+    }
+]);
 
 
